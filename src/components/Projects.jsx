2aea914ddd82691726d7edf2ea8071c1b29a3f65
collins-kimotho <collinskimotho16@gmail.com>
2024-10-18 13:59:11 +0300

    hero-animate

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
         <div>
             {PROJECTS.map((project, index) => (
                 <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                    <div className='w-full lg:1/4'>
+                    <div className='w-full lg:w-1/4'>
                          <img src={project.image} width= {150} height={150}alt={project.title} className='mb-6 rounded'/>
                     </div>
                     <div className='w-full max-w-xl lg:w-3/4'>
