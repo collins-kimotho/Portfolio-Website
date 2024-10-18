690af71e640056df0618b65342daca6f45e2deb5
collins-kimotho <collinskimotho16@gmail.com>
2024-10-18 19:18:47 +0300

    basespash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'Portfolio-Website',
+  base:'/Portfolio-Website',
   plugins: [react()],
   build: {
     rollupOptions: {
