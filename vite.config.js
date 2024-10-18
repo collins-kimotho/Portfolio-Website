2733deedad05e27fef1c878e542419be9c1ae141
collins-kimotho <collinskimotho16@gmail.com>
2024-10-18 19:36:21 +0300

    run

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:'/Portfolio-Website/',
   plugins: [react()],
+  publicDir: 'public',
   build: {
     rollupOptions: {
       external: ['framer-motion']
